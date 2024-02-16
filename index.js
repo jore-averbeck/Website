6cb106945ef8255cbc49ea98e3f4a2f44c7ec4ce
Jore <noa.averbeck@gmail.com>
2024-02-16 13:55:35 +0100

    add mobile project function

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 function toggleMenu() {
-    const menu = document.getElementById("menuLinks");
-    menu.classList.toggle("active");
-    const burger = document.querySelector('.burger-menu');
-    burger.classList.toggle('active');
-  }
+  const menu = document.getElementById("menuLinks");
+  menu.classList.toggle("active");
+  const burger = document.querySelector('.burger-menu');
+  burger.classList.toggle('active');
+}
 
-  document.addEventListener("DOMContentLoaded", function() {
-    const sectionHeading = document.querySelector('.section-heading');
-    sectionHeading.classList.add('loaded'); 
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  const sectionHeading = document.querySelector('.section-heading');
+  sectionHeading.classList.add('loaded'); 
+});
+
+function showTextContainer(containerId, imageID) {
+  const image = document.getElementById(imageID);
+  const textContainer = document.getElementById(containerId);
+  textContainer.classList.toggle("show");
+  image.classList.add("hide");
+}
+
+function hideTextContainer(containerID, imageID){
+  const image = document.getElementById(imageID);
+  const textContainer = document.getElementById(containerID);
+  textContainer.classList.remove("show");
+  image.classList.remove("hide");
+}
