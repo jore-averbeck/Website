bce93f9f803ae4f9ddc2a78a4a6a6557365d4105
Jore <noa.averbeck@gmail.com>
2024-02-17 16:26:45 +0100

    update language selection

--- index.js
+++ index.js
@@ -41,3 +41,53 @@ function scrollFunction() {
     document.getElementById("scrollToTopBtn").style.display = "none";
   }
 }
+
+let currentLanguage = 'en'; 
+let content;
+
+function loadContent(language) {
+  fetch(`content_${language}.json`)
+    .then(response => response.json())
+    .then(data => {
+      content = data;
+      updateContent();
+    })
+    .catch(error => console.error('Error loading content:', error));
+}
+
+function updateContent() {
+  // About Abschnitt aktualisieren
+  document.querySelector('.article-heading').textContent = content.about.heading;
+  document.querySelector('.about-article-heading').textContent = content.about.name;
+  document.querySelector('.about-article ul').innerHTML = content.about.education.map(item => `<li>${item}</li>`).join('');
+  document.querySelector('.about-article-text').textContent = content.about.description;
+
+  // Projekt Abschnitte aktualisieren
+  document.querySelector('.project-section h3').textContent = content.projects.heading;
+  
+  // Projekt 1
+  document.querySelector('#text-container1 h5').textContent = content.projects.project1.title;
+  document.querySelector('#text-container1 p').textContent = content.projects.project1.description;
+
+  // Projekt 2
+  document.querySelector('#text-container2 h5').textContent = content.projects.project2.title;
+  document.querySelector('#text-container2 p').textContent = content.projects.project2.description;
+
+  // Projekt 3
+  document.querySelector('#text-container3 h5').textContent = content.projects.project3.title;
+  document.querySelector('#text-container3 p').textContent = content.projects.project3.description;
+
+  // Kontakt Abschnitt aktualisieren
+  document.querySelector('footer.contact h3').textContent = content.contact.heading;
+  const contactLinks = content.contact.links;
+  document.querySelectorAll('.contact-ul li').forEach((li, index) => {
+    li.innerHTML = `<img src="/img/arrow.svg" height="25px" width="20px" class="arrow"> <a href="${Object.values(contactLinks)[index]}" target="_blank" title="Visit ${Object.keys(contactLinks)[index]}">${Object.values(contactLinks)[index]}</a>`;
+  });
+}
+
+loadContent(currentLanguage);
+
+function switchLanguage(language) {
+  currentLanguage = language;
+  loadContent(language);
+}
